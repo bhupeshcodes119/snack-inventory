d023d8ac8e6ecf50cca34f31e7a22d0fc7571ff3
Bhupesh Chavan <chmcs.2026.bhupesh.chavan@gmail.com>
2026-03-07 12:21:15 +0530

    Update API URL for Render deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Hardcode Render backend URL (your live backend)
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://snack-inventory.onrender.com/snacks", // <-- your backend URL here
 });
 
 export const getSnacks = () => API.get("/");
